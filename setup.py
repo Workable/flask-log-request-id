85cacaae1d828049f22838dfc3cbc38fe0f34bda
Konstantinos Paliouras <paliouras@workable.com>
2017-08-22 14:54:08 +0300

    Bump version to 0.9.0 and updated maintainer info in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
+import re
+import ast
 from setuptools import setup
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('flask_log_request_id/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
 
 setup(
     name='Flask-Log-Request-ID',
@@ -6,9 +16,11 @@ setup(
     url='http://github.com/Workable/flask-log-request-id',
     license='BSD',
     author='Konstantinos Paliouras, Ioannis Foukarakis',
-    author_email='paliouras@workable.com, foukarakis@workable.com',
+    author_email='squarious@gmail.com, foukarakis@workable.com',
     description='An extension that detects request id headers on request '
                 'and propagates on logging module through the life-cycle of request',
+    maintainer="Konstantinos Paliouras",
+    maintainer_email="squarious@gmail.com",
     packages=[
         'flask_log_request_id',
         'flask_log_request_id.extras'],
