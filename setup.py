876e3193213355eae74853a9ecc7db26aec981f3
Konstantinos Paliouras <paliouras@workable.com>
2017-09-26 15:41:24 +0300

    Add option on setup.py to install only test dependency as extra "test"

--- setup.py
+++ setup.py
@@ -94,6 +94,12 @@ with open('flask_log_request_id/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
+test_requirements = [
+        'nose',
+        'mock==2.0.0',
+        'coverage~=4.3.4',
+        'celery~=4.1.0'
+]
 
 setup(
     name='Flask-Log-Request-ID',
@@ -117,15 +123,15 @@ setup(
     install_requires=[
         'Flask>=0.8',
     ],
-    tests_require=[
-        'nose',
-        'mock==2.0.0',
-        'coverage~=4.3.4',
-        'celery~=4.1.0'
-    ],
+    tests_require=test_requirements,
     setup_requires=[
-        "flake8"
+        "flake8",
+        "nose"
     ],
+    extras_require={
+        'test':  test_requirements,
+        'celery': ["celery~=4.1.0"],
+    },
     test_suite='nose.collector',
     classifiers=[
         'Environment :: Web Environment', 'Intended Audience :: Developers',
