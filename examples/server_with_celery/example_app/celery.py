780d34c1ef6919fe1122e6bdd87c56a93d4a77c7
Ari Croock <acroock@isaziconsulting.co.za>
2017-10-24 16:59:34 +0200

    Add example illustrating the use of the celery signal handler

--- examples/server_with_celery/example_app/celery.py
+++ examples/server_with_celery/example_app/celery.py
@@ -1,4 +1,8 @@
-from celery import Celery
-from flask_log_request_id.extras.celery import RequestIDAwareTask
+from celery import Celery, signals
+from flask_log_request_id.extras.celery import add_request_id_header, RequestIDAwareTask
 
+# Either sublcass all tasks from RequestIDAwareTask
 celery = Celery(task_cls=RequestIDAwareTask)
+
+# Or alternatively register the provided signal handler
+signals.before_task_publish.connect(add_request_id_header)
